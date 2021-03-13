34533dbf636d6a4ae58342569faaac5486c351f4
Iskeletu <80495916+Iskeletu@users.noreply.github.com>
2021-03-13 14:19:11 -0300

    Add files via upload

---
+++ sortingAlgorithms.h
@@ -0,0 +1,2 @@
+void bubbleSort(int vector[], int vector_size);
+void swap(int* n1, int* n2);
---
+++ swap.c
@@ -0,0 +1,9 @@
+#include "sortingAlgorithms.h"
+
+//Swaps a n1 integer with a n2 integer.
+void swap(int* n1, int* n2)
+{
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
