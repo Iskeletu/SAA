471310e6dc15da7d218e767e5ec77dd1d6f05a1b
Iskeletu <80495916+Iskeletu@users.noreply.github.com>
2021-03-15 19:44:37 -0300

    Delete sortingAlgorithms.h

--- sortingAlgorithms.h
+++
@@ -1,9 +0,0 @@
-void swap(int* n1, int* n2);
-void bubbleSort(int vector[], int vector_size);
-void selectionSort(int vector[], int vector_size);
-void insertionSort(int vector[], int vector_size);
-void shellSort(int vector[], int vector_size);
-void heapSort(int vector[], int vector_size);
-void mergeSort(int vector[], int vector_size);
-void quickSort(int vector[], int vector_size);
-void countingSort(int vector[], int vector_size);
